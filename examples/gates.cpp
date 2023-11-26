4053033c21f0a7ef6c31b40240495c92d333b64b
Restyled.io <commits@restyled.io>
2023-11-26 18:05:15 +0000

    Restyled by clang-format

--- examples/gates.cpp
+++ examples/gates.cpp
@@ -5,66 +5,64 @@
 #include "Scions/core/op/tensor_op.h"
 #include "Scions/ep/cpu/cpu_ep.h"
 #include "Scions/ep/cpu/mem/mem_manager.h"
-#include "type_traits"
 #include "Scions/ep/cpu/op/execute_op.h"
+#include "type_traits"
 
 using namespace std;
 #define OP_SIZE 2
 
 template<size_t Mem, uint64_t Size>
-consteval auto executeCPUGraph(const auto& graph, const scions::ep::cpu::CpuMemoryManager<Mem, Size>& manager) {
-    using namespace std::chrono;
-    using namespace scions;
-    const auto start = high_resolution_clock::now();
+consteval auto executeCPUGraph(const auto &graph, const scions::ep::cpu::CpuMemoryManager<Mem, Size> &manager) {
+  using namespace std::chrono;
+  using namespace scions;
+  const auto start = high_resolution_clock::now();
 
-    for (const op::OpDesc &op : graph.ops) {
-        ep::cpu::executeOp(op, manager);
-    }
-    const auto end      = high_resolution_clock::now();
-    const auto duration = duration_cast<milliseconds>(end - start);
-    return ep::cpu::CPUExecutionStats{ duration };
+  for (const op::OpDesc &op : graph.ops) { ep::cpu::executeOp(op, manager); }
+  const auto end      = high_resolution_clock::now();
+  const auto duration = duration_cast<milliseconds>(end - start);
+  return ep::cpu::CPUExecutionStats{ duration };
 }
 
 // builds the graph at compile time
 [[nodiscard]] consteval auto buildGraph() {
 
-    // Alright here will be the example start
-    using namespace scions;
+  // Alright here will be the example start
+  using namespace scions;
 
-    static constexpr array mems = {
-        mem::StaticMem(1024, 1024, "inp1"),
-        mem::StaticMem(1024, 1024, "inp2"),
-        mem::StaticMem(1024, 1024, "inp3"),
-    };
+  static constexpr array mems = {
+    mem::StaticMem(1024, 1024, "inp1"),
+    mem::StaticMem(1024, 1024, "inp2"),
+    mem::StaticMem(1024, 1024, "inp3"),
+  };
 
-    constexpr mem::MemDescriptor desc = mem::MemDescriptor(mems);
+  constexpr mem::MemDescriptor desc = mem::MemDescriptor(mems);
 
-    static constexpr std::array<op::OpDesc, OP_SIZE> ops = {
-        op::tensor::TensorAddOpDesc(0, 1, 2),
-        op::tensor::TensorMultiplyOpDesc(0, 3, 2),
-    };
+  static constexpr std::array<op::OpDesc, OP_SIZE> ops = {
+    op::tensor::TensorAddOpDesc(0, 1, 2),
+    op::tensor::TensorMultiplyOpDesc(0, 3, 2),
+  };
 
-    constexpr graph::SequentialGraph graph = { ops, desc };
+  constexpr graph::SequentialGraph graph = { ops, desc };
 
-    return graph;
+  return graph;
 }
 
 int main() {
-    using namespace scions;
-    using namespace scions::ep;
-    // Compile time graph
-    static constexpr auto res   = buildGraph();
-    static constexpr auto &desc = res.memDescriptor;
-    static constexpr auto mem_size  = desc.memoryObjects.size();
-    static constexpr auto by    = desc.getTotalBytes();
+  using namespace scions;
+  using namespace scions::ep;
+  // Compile time graph
+  static constexpr auto res      = buildGraph();
+  static constexpr auto &desc    = res.memDescriptor;
+  static constexpr auto mem_size = desc.memoryObjects.size();
+  static constexpr auto by       = desc.getTotalBytes();
 
-    // set CPUExecutionProvider Options
-    constexpr cpu::CPUOptions options{ true };
-    // Note: Always set this to static if you don't want to blow past
-    // the stack size
-    static auto manager = cpu::CpuMemoryManager<mem_size, by>(desc);
+  // set CPUExecutionProvider Options
+  constexpr cpu::CPUOptions options{ true };
+  // Note: Always set this to static if you don't want to blow past
+  // the stack size
+  static auto manager = cpu::CpuMemoryManager<mem_size, by>(desc);
 
-    // auto provider = cpu::CPUStaticExecutionProvider<OP_SIZE, mem_size, by>(res, manager, options);
-    cpu::CPUExecutionStats const stats = executeCPUGraph(res,manager);
-    return 0;
+  // auto provider = cpu::CPUStaticExecutionProvider<OP_SIZE, mem_size, by>(res, manager, options);
+  cpu::CPUExecutionStats const stats = executeCPUGraph(res, manager);
+  return 0;
 }
--- include/Scions_v2/common/d_type.h
+++ include/Scions_v2/common/d_type.h
@@ -6,32 +6,25 @@
 
 namespace scions::d_type {
 enum TYPE {
-    INT8,
-    INT16,
-    INT32,
-    INT64,
-    F16,
-    F32,
-    F64,
+  INT8,
+  INT16,
+  INT32,
+  INT64,
+  F16,
+  F32,
+  F64,
 };
 
 constexpr uint8_t getDTypeSizeBytes(const TYPE t) {
-    switch (t) {
-    case INT8:
-        return sizeof(int8_t);
-    case INT16:
-        return sizeof(int16_t);
-    case INT32:
-        return sizeof(int32_t);
-    case INT64:
-        return sizeof(int64_t);
-    case F16:
-        return sizeof(int16_t);
-    case F32:
-        return sizeof(float_t);
-    case F64:
-        return sizeof(double);
-    }
+  switch (t) {
+  case INT8: return sizeof(int8_t);
+  case INT16: return sizeof(int16_t);
+  case INT32: return sizeof(int32_t);
+  case INT64: return sizeof(int64_t);
+  case F16: return sizeof(int16_t);
+  case F32: return sizeof(float_t);
+  case F64: return sizeof(double);
+  }
 }
 
-} // namespace scions::d_type
+}  // namespace scions::d_type
